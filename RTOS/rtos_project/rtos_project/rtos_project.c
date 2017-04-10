e8de9716b6fbe8eaba46a688f6c63b6782a4bcf9
AntonHellbe <antonhellbe2@gmail.com>
2017-04-10 14:05:39 +0200

    la till mapp för RTOS projekt atmel

---
+++ RTOS/rtos_project/rtos_project/rtos_project.c
@@ -0,0 +1,24 @@
+/*
+ * rtos_project.c
+ *
+ * Created: 2017-04-10 14:02:56
+ *  Author: Anton
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
