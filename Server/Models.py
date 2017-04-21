1109e1e764124568d75a105258cdac25a85da6a4
AntonHellbe <antonhellbe2@gmail.com>
2017-04-21 13:15:20 +0200

    added simple restapi to post and get coordinates

---
+++ Server/Models.py
@@ -0,0 +1,22 @@
+#########################
+# Används ej efter Gustaf är en hater
+#
+########################
+
+import json
+
+class Coordinates(object):
+    id = 0
+    x = 0
+    y = 0
+
+    def __init__(self, id,  x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4, separators=(':',':'))
+
+    #def as_dict(self):
+    #    return {'id' : self.id, 'x' : self.x, 'y' : self.y}
