f1849c065a92e6db87249789fa811a18ef993e54
arvidsigvardsson <arvid.sigvardsson@gmail.com>
2017-05-04 23:12:01 +0200

    Implementerat homography i coordmapping, det som saknas är höjd

--- opencv/coordmapping.py
+++ opencv/coordmapping.py
@@ -3,75 +3,67 @@
 
 import math
 import numpy as np
+import cv2
+
 class Mapper:
-    def __init__(self, p1, p2, p3, p4, xdim, ydim):
+    def __init__(self, p1, p2, p3, p4, xdim, ydim, camerapos):
         self.xdim = xdim
         self.ydim = ydim
-        self.calibrate(p1, p2, p3, p4)
+        self.calibrate(p1, p2, p3, p4, camerapos)
 
-    def calibrate(self, p1, p2, p3, p4):
-        A = np.matrix(p1 + (1,)).T
-        B = np.matrix(p2 + (1,)).T
-        C = np.matrix(p3 + (1,)).T
-        D = np.matrix(p4 + (1,)).T
+    def calibrate(self, p1, p2, p3, p4, camerapos):
+        # gör om punkterna från tuples till nparrayer
+        a = np.array([[p1[0]], [p1[1]], [1.0]])
+        b = np.array([[p2[0]], [p2[1]], [1.0]])
+        c = np.array([[p3[0]], [p3[1]], [1.0]])
+        d = np.array([[p4[0]], [p4[1]], [1.0]])
 
-        # matris som vänder på y-axeln, så att den växer uppåt, istället för ned som vi får ut från opencv, samt sätter punkten p1 till origo
-        trans = np.matrix(((1, 0, -A.item(0)), (0, -1, A.item(1)), (0, 0, 1)))
+        # matris för att sätta a till origo och vända på yaxeln, som växer positivt nedåt i bildkoordinater. Vi vill ha att den växer positivt uppåt
+        flip_y_origo = np.array([[1,  0, -p1[0]],
+                                 [0, -1,  p1[1]],
+                                 [0,  0,      1]])
 
-        AB = np.matmul(trans, A + 0.5*(B - A))
-        BC = np.matmul(trans, B + 0.5*(C - B))
-        CD = np.matmul(trans, C + 0.5*(D - C))
-        AD = np.matmul(trans, A + 0.5*(D - A))
-#
-#        print 'AB'
-#        print AB
-#        print 'BC'
-#        print BC
-#        print 'CD'
-#        print CD
-#        print 'AD'
-#        print AD
-#
-#        print trans
+        # mappa om hörnen med flip_y_origo
+        A = np.matmul(flip_y_origo, a)
+        B = np.matmul(flip_y_origo, b)
+        C = np.matmul(flip_y_origo, c)
+        D = np.matmul(flip_y_origo, d)
 
-        # trans = np.matrix(([1, 0, -p1[0]], [0, 1, -p1[1]], [0, 0, 1]))
+        # lista med bildkoordinater
+        image_corners = np.array([A[:-1], B[:-1], C[:-1], D[:-1]])
 
-        # baselinevec = np.matrix(([p2[0] - p1[0]], [p2[1] - p1[1]]))
-        baselinevec = BC - AD
-        
-        # print 'trans:'
-        # print trans 
-        # print 'baseline:'
-        # print baselinevec # np.array_str(baselinevec)
+        # lista över de fyra hörnen i världskoordinater
+        # world_corners = np.array([
+        #     np.array([[0],         [0],         [1.0]]),
+        #     np.array([[self.xdim], [0],         [1.0]]),
+        #     np.array([[self.xdim], [self.ydim], [1.0]]),
+        #     np.array([[0],         [self.ydim], [1.0]]) ])
 
-        # xbase = np.array(([1], [0]))
-        xbase = np.matrix((1, 0, 0))
-        # print 'xbase:'
-        # print xbase
+        world_corners = np.array([
+            np.array([[0],         [0.0]]),
+            np.array([[self.xdim], [0]]),
+            np.array([[self.xdim], [self.ydim] ]),
+            np.array([[0],         [self.ydim]]) ])
 
-        #dotp = np.matmul(baselinevec.T, xbase)[0,0] 
-        dotp = np.matmul(xbase, baselinevec).item(0)
-        alpha = math.acos(dotp /(np.linalg.norm(baselinevec) * np.linalg.norm(xbase)))
+        # print 'image'
+        # print image_corners
+        # print 'world'
+        # print world_corners
 
-        # koll om punkterna ska roteras medsols eller motsols
-        if BC.item(1) > AD.item(1):
-            theta = - alpha
-        else:
-            theta = alpha
+        # räkna ut homographymatrisen
+        h, status = cv2.findHomography(image_corners, world_corners)
 
-        rotate = np.matrix(((math.cos(theta), -math.sin(theta), 0), (math.sin(theta), math.cos(theta), 0), (0, 0, 1)))
-        xscale = self.xdim / np.linalg.norm(BC - AD)
-        yscale = self.ydim / np.linalg.norm(AB - CD)
-        scaler = np.matrix(((xscale, 0, 0), (0, yscale, 0), (0, 0, 1)))
+        # print A[:-1]
 
-        self.mapmtx = np.matmul(scaler, np.matmul(rotate, trans))
+        self.mapmtx = np.matmul(h, flip_y_origo)
+        print h
 
     def get_mapped(self, p):
-        point = np.matrix(p + (1,)).T
+        point = np.array([[p[0]], [p[1]], [1]])
         result = np.matmul(self.mapmtx, point)
         return (result.item(0), result.item(1))
 
-# bilateration med två kända punkter, en i origo och den andra på y-axeln med avstånd dist till origo. r1 är okända punktens avstånd till origo, r2 avstånd till andra punkten. Returnerarpositivt x-värde, även -x är lösning
+# bilateration med två kända punkter, en i origo och den andra på y-axeln med avstånd dist till origo. r1 är okända punktens avstånd till origo, r2 avstånd till andra punkten. Returnerarpositivt x-värde, även -x är giltig lösning
 def bilat(dist, r1, r2):
     y = (r1**2 - r2**2 + dist**2) / (2 * dist)
     x = math.sqrt(r1**2 - y**2)
@@ -81,13 +73,21 @@ def bilat(dist, r1, r2):
 def main():
     # dist = 300
     # print bilat(dist, 66.4, 261.1)
-    mp = Mapper((561, 2722), (3426, 2524), (3228, 379), (429, 610), 400, 300)
-    E = (2942, 627)
-    # mp = Mapper((3,6), (10,2),(7,-3.5),(0,0), 4, 3.4)
+    # mp = Mapper((561, 2722), (3426, 2524), (3228, 379), (429, 610), 400, 300)
+    # E = (2942, 627)
+    # # mp = Mapper((3,6), (10,2),(7,-3.5),(0,0), 4, 3.4)
     # newpoint = mp.get_mapped((4,4))
-    newpoint = mp.get_mapped(E)
+    # newpoint = mp.get_mapped(E)
+    # print 'Mappad punkt:', newpoint
+
+    mp = Mapper((335, 3432), (2480, 3239), (2238, 434), (143, 555), 300.0, 400.0, (-1, -1, -1))
+    H = (480, 882)
+    newpoint = mp.get_mapped(H)
     print 'Mappad punkt:', newpoint
 
+    F = (1388, 1740)
+    fp = mp.get_mapped(F)
+    print 'F:', fp
 if __name__ == '__main__':
     main()
 
