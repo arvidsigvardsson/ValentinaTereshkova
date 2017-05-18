b44b1435fa53ff6589cbea04697b9697a5318239
GurraB <gustaf.t.bohlin@gmail.com>
2017-05-18 09:30:11 +0200

    Testad kod för TWI och ESP. Lade till signed int som inte är testat än.

--- Due-kort TWI Arduibo/DueTWI/DueTWI.ino
+++ Due-kort TWI Arduibo/DueTWI/DueTWI.ino
@@ -1,15 +1,14 @@
 #include <Wire.h>
 
-
 uint8_t rx_buff[3];
-uint8_t send_data[5];
-uint8_t byteArray[8];
-uint8_t twi_state = 0,request_state = 0;
-uint8_t led1[5];
-uint8_t led2[5];
-uint8_t strumpa[5];
-uint8_t kub[5];
-uint8_t glas[5];
+int8_t send_data[5];
+int8_t byteArray[8];
+int8_t twi_state = 0,request_state = 0;
+int8_t led1[5];
+int8_t led2[5];
+int8_t strumpa[5];
+int8_t kub[5];
+int8_t glas[5];
 
 
 
@@ -20,101 +19,135 @@ void setup() {
   Wire.onRequest(requestEvent);
   Wire.onReceive(recieveEvent);
   Serial.begin(115200);
-  Serial1.begin(9600);
+  Serial1.begin(115200);
   Serial.println("Starting up twi");
 }
 
 void loop() {
-
+  
   while(Serial1.available()) {
     Serial1.readBytes(byteArray, 5);
     //Serial.println((byteArray[1] << 8) | (byteArray[2] << 0));
     //Serial.println((byteArray[3] << 8) | (byteArray[4] << 0));
-    Serial.println("--------------");
+    
     switch(byteArray[0]) {
       case 0x50:  //lysdiod 1 positon
-        led1[0] = 0x33; 
+        led1[0] = 0x50; 
         led1[1] = byteArray[1];
         led1[2] = byteArray[2];
         led1[3] = byteArray[3];
         led1[4] = byteArray[4];
-        Serial.println("led1");
+        //Serial.println("led1");
       break;
       case 0x51: //lysdiod 2 position
-        led2[0] = 0x34;
+        led2[0] = 0x51;
         led2[1] = byteArray[1];
         led2[2] = byteArray[2];
         led2[3] = byteArray[3];
         led2[4] = byteArray[4];
-        Serial.println("led2");
+        //Serial.println("led2");
       break;
       case 0x52:  //strumpa position
-        strumpa[0] = 0x35;
+        strumpa[0] = 0x52;
         strumpa[1] = byteArray[1];
         strumpa[2] = byteArray[2];
         strumpa[3] = byteArray[3];
         strumpa[4] = byteArray[4];
-        Serial.println("strumpa");
+        //Serial.println("strumpa");
       break;
       case 0x53:  //kubens position
-        kub[0] = 0x36;
+        kub[0] = 0x53;
         kub[1] = byteArray[1];
         kub[2] = byteArray[2];
         kub[3] = byteArray[3];
         kub[4] = byteArray[4];
-        Serial.println("kub");
+        //Serial.println("kub");
       break;
       case 0x54:  //glasets position
-        kub[0] = 0x37;
+        kub[0] = 0x54;
         glas[1] = byteArray[1];
         glas[2] = byteArray[2];
         glas[3] = byteArray[3];
         glas[4] = byteArray[4];
-        Serial.println("glas");
+        //Serial.println("glas");
       break;
     }
+    Serial.println(".");
   }
 }
 
 //skicka datan som har blivit förberedd
 void requestEvent(){
-  Wire.write(send_data, 5);s
+  if(send_data[0] != 0){
+    Wire.write(send_data, 5);
+    for(int n = 0; n < 5; n++) {
+      Serial.println(send_data[n]);
+    }
+  }
 }
 
 //ta emot ett kommando
 void recieveEvent(int TURNDOWNFORWHAT){
-  Serial.println("kommando");
-  int i = 0;
   while(Wire.available()){
     twi_state = Wire.read();
-    rx_buff[i] = twi_state;
-    i++;
+    rx_buff[0] = twi_state;
   }
+  Serial.print("Kommando: ");
+  Serial.println(rx_buff[0], HEX);
   switch(rx_buff[0]){
+    case 0x00:
+      send_data[0] = 0;
+    break;
     case 0x50:  //efterfrågar lysdiod 1 positon
       for(int n = 0; n < 5; n++) {
         send_data[n] = led1[n];
       }
+      //send_data[0] = 0x50;
+      //send_data[1] = 0;
+      //send_data[2] = 200;
+      //send_data[3] = 0;
+      //send_data[4] = 200;
     break;
     case 0x51: //efterfrågar lysdiod 2 position
       for(int n = 0; n < 5; n++) {
         send_data[n] = led2[n];
       }
+      //send_data[0] = 0x51;
+      //send_data[1] = 0;
+      //send_data[2] = 150;
+      //send_data[3] = 0;
+      //send_data[4] = 150;
     break;
     case 0x52:  //efterfrågar strumpa position
-      for(int n = 0; n < 5; n++) {
+      /*for(int n = 0; n < 5; n++) {
         send_data[n] = strumpa[n];
-      }
+      }*/
+
+      send_data[0] = 0x52;
+      send_data[1] = 0;
+      send_data[2] = 100;
+      send_data[3] = 0;
+      send_data[4] = 100;
     break;
     case 0x53:  //efterfrågar kubens position
-      for(int n = 0; n < 5; n++) {
+      /*for(int n = 0; n < 5; n++) {
         send_data[n] = kub[n];
-      }
+      }*/
+      send_data[0] = 0x53;
+      send_data[1] = 0;
+      send_data[2] = 50;
+      send_data[3] = 0;
+      send_data[4] = 50;
     break;
     case 0x54:  //efterfrågar glasets position
-      for(int n = 0; n < 5; n++) {
+      /*for(int n = 0; n < 5; n++) {
         send_data[n] = glas[n];
-      }
+      }*/
+      send_data[0] = 0x54;
+      send_data[1] = 0;
+      send_data[2] = 10;
+      send_data[3] = 0;
+      send_data[4] = 10;
     break;
   }
   
--- ESP8266HTTP/ESP8266HTTP.ino
+++ ESP8266HTTP/ESP8266HTTP.ino
@@ -14,12 +14,12 @@ const char* password = "MAHVMAHV";
 String payload = "";
 HTTPClient http;
 int httpCode;
-uint8_t byteArray[8];
-uint8_t sockArray[5];
-uint8_t cubeArray[5];
-uint8_t glassArray[5];
-uint8_t led1Array[5];
-uint8_t led2Array[5];
+int8_t byteArray[8];
+int8_t sockArray[5];
+int8_t cubeArray[5];
+int8_t glassArray[5];
+int8_t led1Array[5];
+int8_t led2Array[5];
 
 uint8_t newState = 0;
 uint8_t state = 0;
@@ -30,7 +30,7 @@ SoftwareSerial rxtx(12,14);
 void setup() {
   pinMode(CS, OUTPUT);
   Serial.begin(115200);
-  rxtx.begin(9600);
+  rxtx.begin(115200);
   delay(10);
 
   // Connect to WiFi
@@ -67,39 +67,39 @@ void loop() {
       newState = 2;
     break;
     case 1:
-      http.begin("http://192.168.20.111:5000/srv/objectlist");
+      http.begin("http://192.168.20.133:5000/srv/objectlist");
       httpCode = http.GET();
 
       if(httpCode > 0){
         payload = http.getString();
         StaticJsonBuffer<512> jsonBuffer;
         JsonObject& root = jsonBuffer.parseObject(payload);
-        uint16_t sockx = (uint16_t) int(root["sock"][0]["x"]);
-        uint16_t socky = (uint16_t) int(root["sock"][0]["y"]);
-        uint16_t cubex = (uint16_t) int(root["cube"][0]["x"]);
-        uint16_t cubey = (uint16_t) int(root["cube"][0]["y"]);
-        uint16_t glassx = (uint16_t) int(root["glas"][0]["x"]);
-        uint16_t glassy = (uint16_t) int(root["glas"][0]["y"]);
+        int16_t sockx = (int16_t) int(root["sock"][0]["x"]);
+        int16_t socky = (int16_t) int(root["sock"][0]["y"]);
+        int16_t cubex = (int16_t) int(root["cube"][0]["x"]);
+        int16_t cubey = (int16_t) int(root["cube"][0]["y"]);
+        int16_t glassx = (int16_t) int(root["glas"][0]["x"]);
+        int16_t glassy = (int16_t) int(root["glas"][0]["y"]);
         sockArray[0] = 0x52;
         delay(1);
-        sockArray[1] = (uint8_t) (sockx >> 8);
-        sockArray[2] = (uint8_t) (sockx);
-        sockArray[3] = (uint8_t) (socky >> 8);
-        sockArray[4] = (uint8_t) (socky);
+        sockArray[1] = (int8_t) (sockx >> 8);
+        sockArray[2] = (int8_t) (sockx);
+        sockArray[3] = (int8_t) (socky >> 8);
+        sockArray[4] = (int8_t) (socky);
         rxtx.write(sockArray, 5);
         delay(1);
         cubeArray[0] = 0x53;
-        cubeArray[1] = (uint8_t) (cubex >> 8);
-        cubeArray[2] = (uint8_t) (cubex);
-        cubeArray[3] = (uint8_t) (cubey >> 8);
-        cubeArray[4] = (uint8_t) (cubey);
+        cubeArray[1] = (int8_t) (cubex >> 8);
+        cubeArray[2] = (int8_t) (cubex);
+        cubeArray[3] = (int8_t) (cubey >> 8);
+        cubeArray[4] = (int8_t) (cubey);
         rxtx.write(cubeArray, 5);
         delay(1);
         glassArray[0] = 0x54;
-        glassArray[1] = (uint8_t) (glassx >> 8);
-        glassArray[2] = (uint8_t) (glassx);
-        glassArray[3] = (uint8_t) (glassy >> 8);
-        glassArray[4] = (uint8_t) (glassy);
+        glassArray[1] = (int8_t) (glassx >> 8);
+        glassArray[2] = (int8_t) (glassx);
+        glassArray[3] = (int8_t) (glassy >> 8);
+        glassArray[4] = (int8_t) (glassy);
         rxtx.write(glassArray, 5);
         newState = 2;
         Serial.println("sent obj");
@@ -109,36 +109,41 @@ void loop() {
       }
     break;
     case 2:  
-      http.begin("http://192.168.20.111:5000/srv/coordinate/getlatest");
+      http.begin("http://192.168.20.133:5000/srv/coordinate/getlatest");
       httpCode = http.GET();
   
       if(httpCode > 0){
         payload = http.getString();
         StaticJsonBuffer<512> jsonBuffer;
         JsonObject& root = jsonBuffer.parseObject(payload);
-        uint16_t x_1 = (uint16_t) int(root["coordinate"]["x1"]);
-        uint16_t y_1 = (uint16_t) int(root["coordinate"]["y1"]);
-        uint16_t x_2 = (uint16_t) int(root["coordinate"]["x2"]);
-        uint16_t y_2 = (uint16_t) int(root["coordinate"]["y1"]);
+        int16_t x_1 = (int16_t) int(root["coordinate"]["x1"]);
+        int16_t y_1 = (int16_t) int(root["coordinate"]["y1"]);
+        int16_t x_2 = (int16_t) int(root["coordinate"]["x2"]);
+        int16_t y_2 = (int16_t) int(root["coordinate"]["y2"]);
         if(x_1 < 0) {
           x_1 = 0;
         }
         if(y_1 < 0) {
           y_1 = 0;
         }
-
+        if(x_2 < 0) {
+          x_1 = 0;
+        }
+        if(y_2 < 0) {
+          y_1 = 0;
+        }
         led1Array[0] = 0x50;
-        led1Array[1] = (uint8_t) (x_1 >> 8);
-        led1Array[2] = (uint8_t) (x_1);
-        led1Array[3] = (uint8_t) (y_1 >> 8);
-        led1Array[4] = (uint8_t) (y_1);
+        led1Array[1] = (int8_t) (x_1 >> 8);
+        led1Array[2] = (int8_t) (x_1);
+        led1Array[3] = (int8_t) (y_1 >> 8);
+        led1Array[4] = (int8_t) (y_1);
         rxtx.write(led1Array, 5);
         delay(1);
         led2Array[0] = 0x51;
-        led2Array[1] = (uint8_t) (x_2 >> 8);
-        led2Array[2] = (uint8_t) (x_2);
-        led2Array[3] = (uint8_t) (y_2 >> 8);
-        led2Array[4] = (uint8_t) (y_2);
+        led2Array[1] = (int8_t) (x_2 >> 8);
+        led2Array[2] = (int8_t) (x_2);
+        led2Array[3] = (int8_t) (y_2 >> 8);
+        led2Array[4] = (int8_t) (y_2);
         rxtx.write(led2Array, 5);
         Serial.println("sent coord");
       }
@@ -146,6 +151,6 @@ void loop() {
     break;
   }
   state = newState;
-  delay(100);
+  delay(400);
 
 }
